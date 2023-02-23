d8fcfc7e4252f7d5c5507c39d66c0c5267d8f65a
FallMaple <tony53517230@gmail.com>
2023-02-24 03:41:51 +0800

    fix: 誤將fcitx主題當作rofi主題，並將Nord-Dark主題加入

--- refresh.sh
+++ refresh.sh
@@ -9,9 +9,8 @@ cp ~/.config/polybar/config.ini polybar/
 cp ~/.config/polybar/launch.sh polybar/
 cp -r ~/.config/polybar/scripts polybar/
 
-# rofi theme
-cp /usr/share/fcitx5/themes/FluentDark/theme.conf rofi/FluentDark/
-
 # fcitx5
 cp -r ~/.config/fcitx5/conf fcitx5/
 cp ~/.config/fcitx5/config fcitx5/
+# fcitx5 theme Nord-Dark
+cp -r ~/.local/share/fcitx5/themes/Nord-Dark/ fcitx5/theme
